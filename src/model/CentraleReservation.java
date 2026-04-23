62cf361546b0cd8b6b069e33b75a2c3793f23bd7
RemiA53 <remialibert690@gmail.com>
2026-04-23 19:02:43 +0200

    Entite CentraleReservation

---
+++ src/model/CentraleReservation.java
@@ -0,0 +1,34 @@
+package model;
+
+public class CentraleReservation<F extends Formulaire, E extends EntiteReservable<Formulaire>> {
+	private E[] entites;
+	private int nbEntite;
+	
+	public CentraleReservation(E[] entites) {
+		this.entites = entites;
+	}
+	
+	public int ajouterEntite(E entite) {
+		entites[nbEntite] = entite;
+		return nbEntite++;
+	}
+	
+	public int[] donnerPossibilites(F formulaire) {
+		int[] possibilites = new int[nbEntite];
+		for (int i = 0; i < nbEntite; i++) {
+			boolean estLibre = entites[i].estLibre(formulaire);
+			if (estLibre) {
+				possibilites[i] = 1;
+			} else {
+				possibilites[i] = 0;
+			}
+		}
+		return possibilites;
+	}
+	
+	public Reservation Reservation(int numEntite, F formulaire) {
+		E entite = entites[numEntite-1];
+		formulaire.setIdentificationEntite(numEntite);
+		return entite.reserver(formulaire);
+	}
+}
